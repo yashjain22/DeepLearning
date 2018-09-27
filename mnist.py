8d8cceda8bbf237fee722f9d3fdcb0aefb5da46b
Yash Jain <yashjainudz@gmail.com>
2018-09-27 11:26:09 +0530

    added comments and remoed obselete code

--- mnist.py
+++ mnist.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 #sys.stdin.flush()
-train,test = mnist.load_data()
+train,test = mnist.load_data() # load data from keras.datasets
 train_x,train_y = train
 train_x2 = train_x
 # start
@@ -24,20 +24,20 @@ test_x,test_y = test
 test_x2 = test_x
 shape1 = train_x.shape
 shape2 = test_x.shape
-train_x = train_x.reshape(shape1[0],shape1[1]*shape1[2])
-test_x = test_x.reshape(shape2[0],shape2[1]*shape2[2])
-train_x = train_x / 255
-test_x = test_x / 255
-train_y = np_utils.to_categorical(train_y)
-test_y = np_utils.to_categorical(test_y)
-print(train_x[14].shape)
+train_x = train_x.reshape(shape1[0],shape1[1]*shape1[2]) # flatten the train images 
+test_x = test_x.reshape(shape2[0],shape2[1]*shape2[2])  # flatten the test images
+train_x = train_x / 255     # normalize train data
+test_x = test_x / 255       # normalize test data
+train_y = np_utils.to_categorical(train_y) # one hot-encoding
+test_y = np_utils.to_categorical(test_y)  # one hot-encoding
+# validate shapes of our train and test numpy arrays
 print(train_x.shape)
 print(train_y.shape)
 print(test_x.shape)
 print(test_y.shape)
 
 model = Sequential( [
-    Dense(784,input_shape=(784,)),
+    Dense(784,input_shape=(784,)),  
     Activation('relu'),
     Dense(50),
     Activation('relu'),
@@ -46,17 +46,18 @@ model = Sequential( [
     Dense(10),
     Activation('softmax'),
 ] )
-model.summary()
+model.summary() # overview of our neural architecture
 model.compile(Adam(lr=0.005),loss="categorical_crossentropy",metrics=['accuracy'])
-model.fit(train_x,train_y,epochs=10,batch_size=250)
+model.fit(train_x,train_y,epochs=10,batch_size=250) # train using training data
 score = model.evaluate(test_x,test_y)
-print("Accuracy is : {}".format(score[1]*100) )
+print("Accuracy is : {}".format(score[1]*100) ) # scores is a list comprising with two float values one is loss and second is the accuracy
 #model.fit()
-print(train_x[14].shape)
-
+"""
+# test with single image and see result yourself
 for i in range(1,10):
     p=random.randint(1,1000)
     predictions=model.predict(np.array([test_x[p]]) )
     print(np.argmax(predictions))
     plt.imshow(test_x2[p], cmap=plt.get_cmap('gray'))
     plt.show()
+"""
